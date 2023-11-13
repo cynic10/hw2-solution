d5dbed6d8149fc11b754ede03d91323e1b72d7c0
Riya Deshpande <riyadeshpande10@gmail.com>
2023-11-13 15:46:54 -0500

    Updated the test cases to the TestExample file

--- expense_tracker/test/TestExample.java
+++ expense_tracker/test/TestExample.java
@@ -2,10 +2,16 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import java.util.Date;
 import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +19,8 @@ import org.junit.Test;
 import controller.ExpenseTrackerController;
 import model.ExpenseTrackerModel;
 import model.Transaction;
+import model.Filter.AmountFilter;
+import model.Filter.CategoryFilter;
 import view.ExpenseTrackerView;
 
 
@@ -111,6 +119,141 @@ public class TestExample {
         // Check the total cost after removing the transaction
         double totalCost = getTotalCost();
         assertEquals(0.00, totalCost, 0.01);
+    }
+    
+    @Test
+    public void testAddTransactionView() {
+      
+    // Pre-condition: List of transactions is empty
+     assertEquals(0, model.getTransactions().size());
+      
+      // Add a transaction
+      assertTrue(controller.addTransaction(50.0, "food"));
+      
+      
+      //From the JTable, check the row count and values at individual cells
+      assertEquals(2, view.getTransactionsTable().getRowCount());
+      assertEquals(50.0, view.getTransactionsTable().getValueAt(0,1));
+      assertEquals("food", view.getTransactionsTable().getValueAt(0,2));
+      
+      //Check for total
+      assertEquals(50.0, view.getTransactionsTable().getValueAt(1,3));
+ 
+      //Check for timestamp value to be not null
+      assertNotNull(view.getTransactionsTable().getValueAt(0,3));
+      
+    }
+    
+    
+    @Test
+    public void testInvalidInputHandling() {
+      assertEquals(0, model.getTransactions().size());
+
+      assertFalse(controller.addTransaction(-10.0, "food"));
+      assertFalse(controller.addTransaction(1001.0, "food"));
+      assertFalse(controller.addTransaction(800.0, "invalid"));
+      assertFalse(controller.addTransaction(2200.0, "invalid"));
+
+      assertEquals(0, model.getTransactions().size());
+      assertEquals(0.0, getTotalCost(), 0.01);
+    }
+    
+   
+	@Test
+    public void testFilterByAmount(){
+    	assertEquals(0, model.getTransactions().size());
+    	
+    	assertTrue(controller.addTransaction(50.0, "food"));
+    	assertTrue(controller.addTransaction(150.0, "food"));
+    	assertTrue(controller.addTransaction(50.0, "other"));
+    	
+    	double amountFilterInput = 50.0;
+    	
+    	AmountFilter amountFilter = new AmountFilter(amountFilterInput);
+    	
+    	 controller.setFilter(amountFilter);
+         
+         
+         List<Transaction> transactions = model.getTransactions();
+         List<Transaction> filteredTransactions = amountFilter.filter(transactions);
+         
+         assertEquals(2, filteredTransactions.size());
+         
+         for(Transaction transaction : filteredTransactions){
+             
+        	 assertEquals(50.0, transaction.getAmount(), 0.01);
+         } 	
+    	
+    }
+	
+	@Test
+    public void testFilterByCategory(){
+    	assertEquals(0, model.getTransactions().size());
+    	
+    	assertTrue(controller.addTransaction(50.0, "food"));
+    	assertTrue(controller.addTransaction(150.0, "food"));
+    	assertTrue(controller.addTransaction(50.0, "other"));
+    	
+    	String categoryFilterInput = "food";
+        CategoryFilter categoryFilter = new CategoryFilter(categoryFilterInput);
+    	
+    	 controller.setFilter(categoryFilter);
+         
+         
+         List<Transaction> transactions = model.getTransactions();
+         List<Transaction> filteredTransactions = categoryFilter.filter(transactions);
+         
+         assertEquals(2, filteredTransactions.size());
+         
+         for(Transaction transaction : filteredTransactions){
+             
+        	 assertTrue(transaction.getCategory().equalsIgnoreCase(categoryFilterInput));
+         } 	
+    	
+    }
+    
+    @Test
+    public void testUndoDisallowed() {
+    	
+    	assertEquals(0, model.getTransactions().size());
+    	
+    	
+    	assertTrue(controller.addTransaction(50.0, "food"));
+    	
+    	JButton button = view.getUndoBtn();
+    	
+    	assertTrue(button.isEnabled());
+    	
+    	controller.undoTransaction();
+    	
+    	assertFalse(button.isEnabled());	
+    	
+    }
+    
+    
+    @Test
+    public void testUndoAllowed() {
+    	
+    	assertEquals(0, model.getTransactions().size());
+    	
+    	assertTrue(controller.addTransaction(50.0, "food"));
+    	assertTrue(controller.addTransaction(150.0, "food"));
+    	
+    	
+    	assertEquals(3, view.getTransactionsTable().getRowCount());
+    	assertEquals(200.0, view.getTransactionsTable().getValueAt(2,3));
+    	
+    	JButton button = view.getUndoBtn();
+    	
+    	assertTrue(button.isEnabled());
+    	
+    	controller.undoTransaction();
+    	
+    	assertEquals(2, view.getTransactionsTable().getRowCount());
+    	assertEquals(50.0, view.getTransactionsTable().getValueAt(1,3));
+    	assertEquals(50.0, view.getTransactionsTable().getValueAt(0,1));
+        assertEquals("food", view.getTransactionsTable().getValueAt(0,2));
+    	
     }
     
 }
